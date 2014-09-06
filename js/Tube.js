b9d955f9eb043684c64ac1f55c8e575ea02fe796
renasboy <renasboy@gmail.com>
2014-09-06 16:58:10 +0200

    Positioned the drop body, tube collision only comming from top

--- js/Tube.js
+++ js/Tube.js
@@ -47,6 +47,9 @@ Game.Tube.prototype.constructor = Game.Tube;
 Game.Tube.prototype.build = function () {
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.setSize(2, 1, this.size / 2 - 1, 0); //collision area
+    this.body.checkCollision.left = false;
+    this.body.checkCollision.right = false;
+    this.body.checkCollision.down = false;
     this.body.velocity.x = this.speed;
     this.body.bounce = new Phaser.Pointer(0, 0);
 };
