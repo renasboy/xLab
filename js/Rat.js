c20f36a0660e952ec9d8900026d3ab0ca997a243
renasboy <renasboy@gmail.com>
2014-09-17 19:25:46 +0200

    Improved rat and tube collisions

--- js/Rat.js
+++ js/Rat.js
@@ -6,12 +6,13 @@ Game.Rat = function (game, rat, x, speed) {
 
     this.rollingPosition = 32;
 
-    this.size = 174;
+    this.imgWidth = 128;
+    this.imgHeight = 174;
 
     // need this
     this.game = game;
 
-    Phaser.Sprite.call(this, game, -this.initialPosition, this.game.height - this.size - this.rollingPosition + 20, this.imgRat, 0);
+    Phaser.Sprite.call(this, game, -this.initialPosition, this.game.height - this.imgHeight - this.rollingPosition + 20, this.imgRat, 0);
     this.animations.add('sleep');
     this.animations.play('sleep', 6, true);
 
@@ -27,7 +28,10 @@ Game.Rat.prototype.constructor = Game.Rat;
 
 Game.Rat.prototype.build = function () {
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
-    this.body.setSize(100, 70, this.size / 2 - 50, this.size - 70); //collision area
+    this.body.setSize(70, 1, this.imgWidth / 2 - 35, this.imgHeight - 70); //collision area
+    this.body.checkCollision.left = false;
+    this.body.checkCollision.right = false;
+    this.body.checkCollision.down = false;
     this.body.velocity.x = this.speed;
     this.body.bounce = new Phaser.Pointer(0, 0);
 };
@@ -52,7 +56,7 @@ Game.Rat.prototype.update = function () {
     if (this.body.x > this.game.width) {
         this.body.x = -this.initialPosition;
     }
-    if (this.body.y - 58 + this.size  + 20 > this.game.height - this.rollingPosition) {
+    if (this.body.y - 58 + this.imgHeight  + 20 > this.game.height - this.rollingPosition) {
         this.body.y = this.game.height - 70 - this.rollingPosition + 20;
     }
 };
